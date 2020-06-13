dec392d77f735e7185e7c260de4354110d3417d5
Sandro Simas <sandro.dacosta@uala.com.ar>
2020-06-13 19:50:53 -0300

    First commit

---
+++ tasks/publish.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+'use strict';
+
+const path = require('path');
+const {commands, publish} = require('release-n-publish');
+
+const WORKING_DIR = path.resolve();
+
+async function lintProject() {
+  commands.log('Linting project...');
+  await commands.exec('npm run lint', WORKING_DIR);
+}
+
+// Run this if call directly from command line
+if (require.main === module) {
+  publish.setWorkingDir(WORKING_DIR);
+  publish.setLintTask(lintProject);
+  publish.setNpmProject(true);
+  publish.run();
+}
---
+++ tasks/release.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+'use strict';
+
+const path = require('path');
+const {commands, release} = require('release-n-publish');
+
+const WORKING_DIR = path.resolve();
+
+async function lintProject() {
+  commands.log('Linting project...');
+  await commands.exec('npm run lint', WORKING_DIR);
+}
+
+// Run this if call directly from command line
+if (require.main === module) {
+  release.setWorkingDir(WORKING_DIR);
+  release.setLintTask(lintProject);
+  release.run(process.argv[2]);
+}
